ab1b72a4eb8255096507f1a9ed7729328574fad4
Hernus Carelsen <hernus@users.noreply.github.com>
2020-07-08 08:37:49 +1000

    Create index.js

---
+++ bappocoding1/010helloworld/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "bappo-components";
+
+export default function Hello1() {
+  return (
+    <View style={{ border: "1px solid blue", margin: 50 }}>
+      <Text>Hello World1</Text>
+    </View>
+  );
+}
